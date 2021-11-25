f4303220446501338ce48b0a517b20011dae1b8f
zshnb <a857681664@gmail.com>
2021-11-25 18:50:53 +0800

    use external database in test

--- src/test/java/com/zshnb/userservice/AddUserTest.java
+++ src/test/java/com/zshnb/userservice/AddUserTest.java
@@ -13,7 +13,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.lang.reflect.ParameterizedType;
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
