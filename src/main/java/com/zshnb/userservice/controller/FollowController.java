b2067f1000999cb6be43fdf3c1e69405aeafbe7d
zshnb <a857681664@gmail.com>
2021-11-27 16:08:04 +0800

    add dependency

--- src/main/java/com/zshnb/userservice/controller/FollowController.java
+++ src/main/java/com/zshnb/userservice/controller/FollowController.java
@@ -1,8 +1,10 @@
 package com.zshnb.userservice.controller;
 
+import com.ejlchina.okhttps.OkHttps;
 import com.zshnb.userservice.common.ListResponse;
 import com.zshnb.userservice.common.Response;
 import com.zshnb.userservice.entity.User;
+import com.zshnb.userservice.exception.PermissionDenyException;
 import com.zshnb.userservice.request.FollowUserRequest;
 import com.zshnb.userservice.request.ListFollowUserRequest;
 import com.zshnb.userservice.request.UnfollowUserRequest;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class FollowController {
+    private final String serverUrl = "http://localhost:8080";
     private final FollowServiceImpl followService;
 
     public FollowController(FollowServiceImpl followService) {
@@ -28,7 +31,26 @@ public class FollowController {
     @GetMapping("/follow/{userId}/follow-users")
     public ListResponse<User> listFollowUser(@RequestParam(defaultValue = "1") int pageNumber,
                                              @RequestParam(defaultValue = "20") int pageSize,
+                                             @RequestParam String code,
+                                             @RequestParam String clientSecret,
                                              @PathVariable int userId) {
+        String str = OkHttps.sync(serverUrl + "/oauth2/token")
+            .addBodyPara("grant_type", "authorization_code")
+            .addBodyPara("code", code)
+            .addBodyPara("client_id", userId)
+            .addBodyPara("client_secret", clientSecret)
+            .post()
+            .getBody()
+            .toString();
+
+        SoMap so = SoMap.getSoMap().setJsonString(str);
+        System.out.println("返回结果: " + so);
+
+        // code不等于200  代表请求失败
+        if(so.getInt("code") != 200) {
+            throw new PermissionDenyException();
+        }
+
         ListFollowUserRequest request = new ListFollowUserRequest();
         request.setUserId(userId);
         request.setPageNumber(pageNumber);
